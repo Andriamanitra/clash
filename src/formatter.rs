6e27bd618c775ff3f416604ef206cd95a7f4297b
Andriamanitra <mikko77792@gmail.com>
2023-11-09 06:37:45 +0200

    look, no regex

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,107 +1,99 @@
 use ansi_term::Style;
-use lazy_static::lazy_static;
-use regex::Regex;
 
 use crate::outputstyle::OutputStyle;
 
-// use lazy_static! to make sure regexes are only compiled once
-lazy_static! {
-    static ref RE_VARIABLE: Regex = Regex::new(r"\[\[((?s).*?)\]\]").unwrap();
-    static ref RE_CONSTANT: Regex = Regex::new(r"\{\{((?s).*?)\}\}").unwrap();
-    static ref RE_BOLD: Regex = Regex::new(r"<<((?s).*?)>>").unwrap();
-    static ref RE_MONOSPACE: Regex = Regex::new(r"`([^`]*?)`").unwrap();
-    static ref RE_MONOSPACE_TRIM: Regex = Regex::new(r"\n? *(`[^`]*`) *").unwrap();
-    static ref RE_BACKTICK: Regex = Regex::new(r"(`[^`]+`)|([^`]+)").unwrap();
-    static ref RE_SPACES: Regex = Regex::new(r" +").unwrap();
-    static ref RE_NONWHITESPACE: Regex = Regex::new(r"[^\r\n ]+").unwrap();
-}
-
-/// Format Codingame statement that contains special formatting syntax
-/// [[VARIABLE]] - {{CONSTANT}} - <<BOLD>> - `MONOSPACE`
 pub fn format_cg(text: &str, ostyle: &OutputStyle) -> String {
-    // Trim consecutive spaces (imitates html behaviour)
-    // But only if it's not in a Monospace block (between backticks ``)
-    let mut result = RE_BACKTICK
-        .replace_all(text, |caps: &regex::Captures| {
-            if let Some(backtick_text) = caps.get(1) {
-                backtick_text.as_str().to_string()
-            } else if let Some(non_backtick_text) = caps.get(2) {
-                RE_SPACES.replace_all(non_backtick_text.as_str(), " ").to_string()
-            } else {
-                "".to_string()
+    let tag_pairs = vec![
+        (ostyle.monospace, "`", "`"),
+        (ostyle.variable, "[[", "]]"),
+        (ostyle.constant, "{{", "}}"),
+        (ostyle.bold, "<<", ">>"),
+    ];
+    let default_style = Style::default();
+    let mut sections: Vec<(Style, usize, usize)> = vec![];
+    let mut stack: Vec<(Style, &str)> = vec![];
+    let mut section_start = 0;
+
+    for (i, ch) in text.char_indices() {
+        if i < section_start {
+            continue
+        }
+        if ch == '\n' {
+            let style = match stack.last() {
+                Some((style, _)) => style,
+                None => &default_style,
+            };
+            sections.push((style.to_owned(), section_start, i));
+            sections.push((default_style, i, i+1));
+            section_start = i + 1;
+            continue
+        }
+        let slice = &text[i..];
+        let found_closing = match stack.last() {
+            Some((style, closing)) if slice.starts_with(closing) => {
+                sections.push((style.to_owned(), section_start, i));
+                section_start = i + closing.len();
+                true
             }
-        })
-        .to_string();
+            _ => {
+                for (style, tag_open, tag_close) in &tag_pairs {
+                    if slice.starts_with(tag_open) {
+                        match stack.last() {
+                            Some((prev_style, _)) => {
+                                sections.push((prev_style.to_owned(), section_start, i));
+                                stack.push((inner_style(prev_style, style), tag_close));
+                            }
+                            None => {
+                                sections.push((default_style.to_owned(), section_start, i));
+                                stack.push((style.to_owned(), tag_close));
+                            }
+                        }
+                        section_start = i + tag_open.len();
+                    } else if slice.starts_with(tag_close) {
+                        println!("WARNING: invalid closing token \"{}\" at {}", tag_close, i);
+                    }
+                }
+                false
+            }
+        };
+        if found_closing {
+            stack.pop();
+        }
+    }
+    if !stack.is_empty() {
+        println!("WARNING: sections without closing tags");
+    }
+    if section_start < text.len() {
+        sections.push((default_style, section_start, text.len()))
+    }
 
-    // Make sure monospace blocks are on their own line, and remove extra
-    // whitespace around them
-    result = RE_MONOSPACE_TRIM
-        .replace_all(&result, |caps: &regex::Captures| format!("\n{}\n", &caps[1]))
-        .to_string();
+    let mut result = String::new();
 
-    // Nested tags (only some combinations).
-    // Hacky - it's based upon the fact that only 1-level nesting makes sense.
-    // Adds reverse nester brackets so that the following replacement logic will
-    // work. i.e : <<Next [[N]] {{3}} lines:>> becomes <<Next >>[[N]]<< {{3}}
-    // lines:>>
-
-    // <<Next [[N]] {{3}} lines:>>
-    result = RE_BOLD
-        .replace_all(&result, |caps: &regex::Captures| {
-            let escaped_vars = RE_VARIABLE
-                .replace_all(&caps[0], |inner_caps: &regex::Captures| format!(">>{}<<", &inner_caps[0]))
-                .to_string();
-            RE_CONSTANT
-                .replace_all(&escaped_vars, |inner_caps: &regex::Captures| format!(">>{}<<", &inner_caps[0]))
-                .to_string()
-        })
-        .to_string();
-
-    // `Next [[N]] {{3}} lines:`
-    result = RE_MONOSPACE
-        .replace_all(&result, |caps: &regex::Captures| {
-            let escaped_vars = RE_VARIABLE
-                .replace_all(&caps[0], |inner_caps: &regex::Captures| format!("`{}`", &inner_caps[0]))
-                .to_string();
-            RE_CONSTANT
-                .replace_all(&escaped_vars, |inner_caps: &regex::Captures| format!("`{}`", &inner_caps[0]))
-                .to_string()
-        })
-        .to_string();
-
-    // {{Next [[N]] `Mono \n and more` lines}}
-    result = RE_CONSTANT
-        .replace_all(&result, |caps: &regex::Captures| {
-            let escaped_cons = RE_VARIABLE
-                .replace_all(&caps[0], |inner_caps: &regex::Captures| {
-                    format!("{}{}{}", "}}", &inner_caps[0], "{{")
-                })
-                .to_string();
-            RE_MONOSPACE
-                .replace_all(&escaped_cons, |inner_caps: &regex::Captures| {
-                    format!("{}{}{}", "}}", &inner_caps[0], "{{")
-                })
-                .to_string()
-        })
-        .to_string();
-
-    result = RE_VARIABLE
-        .replace_all(&result, |caps: &regex::Captures| ostyle.variable.paint(&caps[1]).to_string())
-        .to_string();
-
-    result = RE_CONSTANT
-        .replace_all(&result, |caps: &regex::Captures| ostyle.constant.paint(&caps[1]).to_string())
-        .to_string();
-
-    result = RE_BOLD
-        .replace_all(&result, |caps: &regex::Captures| ostyle.bold.paint(&caps[1]).to_string())
-        .to_string();
-
-    result = RE_MONOSPACE
-        .replace_all(&result, |caps: &regex::Captures| ostyle.monospace.paint(&caps[1]).to_string())
-        .to_string();
+    for (style, a, b) in sections {
+        if a == b { continue }
+        let styled_text = style.paint(&text[a..b]).to_string();
+        // println!("{:2}..{:2} | \"{}\" ({:?})", a, b, &styled_text.replace('\n', "\\n"), style);
+        result += &styled_text;
+    }
 
     result
+}
+
+fn inner_style(outer: &Style, inner: &Style) -> Style {
+    let mut style = inner.to_owned();
+    if style.background.is_none() {
+        style.background = outer.background;
+    }
+    style
+}
+
+#[test]
+fn asd() {
+    //let original = "aa`bb[[cc<<dd>>ee]]f`FF`f{{gg}}hh`ii";
+    let original = "aa\n\n`bb\ncccc\nddd\n`\n\n\neee";
+    println!("{}", original);
+    let styled = format_cg(original, &OutputStyle::default());
+    println!("{}", styled);
 }
 
 /// Replaces spaces with "•" and newlines with "⏎" and paints them with
@@ -109,10 +101,27 @@ pub fn format_cg(text: &str, ostyle: &OutputStyle) -> String {
 pub fn show_whitespace(text: &str, style: &Style, ws_style: &Style) -> String {
     let newl = format!("{}\n", ws_style.paint("⏎"));
     let space = format!("{}", ws_style.paint("•"));
-    let fmt_non_ws = RE_NONWHITESPACE
-        .replace_all(text, |caps: &regex::Captures| style.paint(&caps[0]).to_string())
-        .to_string();
-    fmt_non_ws.replace('\n', &newl).replace(' ', &space)
+    let mut result = String::new();
+    let mut buf = String::new();
+    for ch in text.chars() {
+        match ch {
+            '\n' => {
+                result += &style.paint(&buf);
+                result += &newl;
+                buf.clear();
+            }
+            ' ' => {
+                result += &style.paint(&buf);
+                result += &space;
+                buf.clear();
+            }
+            c => {
+                buf.push(c)
+            }
+        }
+    }
+    result += &style.paint(&buf);
+    result
 }
 
 #[cfg(test)]
@@ -182,10 +191,4 @@ mod tests {
         assert_eq!(formatted_text, expected);
     }
 
-    #[test]
-    fn matches_newlines_bold() {
-        let text = "<<Bold text spread \n across two lines:>>";
-
-        assert_eq!(RE_BOLD.is_match(&text), true);
-    }
 }
